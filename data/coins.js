b165fb462febb488ea292ba3601addf209e22b73
MrFranksJr <frankyjr@me.com>
2023-04-18 18:22:47 +0200

    updates

---
+++ data/coins.js
@@ -0,0 +1,13 @@
+export { coinsToFetch, currentCurrency }
+
+const coinsToFetch = [
+    'bitcoin',
+    'ethereum',
+    'litecoin',
+    'tether',
+    'matic-network',
+    'ripple',
+    'chainlink'
+]
+
+const currentCurrency = 'eur'
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-import { setBackGround, updatePixelRatio, getFullDate, getTime } from "./data/utils.js"
+import { setBackGround, updatePixelRatio, getFullDate, getTime, onloadFetchCrypto, getLocationWeather } from "./data/utils.js"
 
 function initLoad() {
     //Check screen/pixel ratio
     updatePixelRatio()
     //set background
     setBackGround()
+    onloadFetchCrypto()
     document.getElementById('current-date').textContent = getFullDate()
     getTime()
+    getLocationWeather()
 }
 
+
+
 initLoad()
-setInterval(()=> getTime(), 5000)
+setInterval(()=> getTime(), 1000)
