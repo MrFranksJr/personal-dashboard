5917052bd18ef06f133d3b6b78cfefaf99db2564
MrFranksJr <frankyjr@me.com>
2023-04-19 21:51:22 +0200

    new thingz

--- data/trumpquote.js
+++ data/trumpquote.js
@@ -6,7 +6,7 @@ async function fetchTrumpQuote() {
 
     document.getElementById('lowercenter').innerHTML = `
     <a href='${trumpData._links.self.href}' alt='source of quote' target='_blank'>
-        <p class='quote'>" ${trumpData.value}" <br>
+        <p class='quote'>" ${trumpData.value} "<br>
         <span class='signed'> – Mr. Donald J. Trump – &nbsp;<br>45th President of the USA</span>
         </p>
     </a>
--- index.js
+++ index.js
@@ -18,17 +18,37 @@ function initLoad() {
 
     setTimeout(() => {
         document.getElementById('crypto-div').style.backgroundColor = '#00000040'
-        document.getElementById('crypto-div').onmouseover = () => document.getElementById('crypto-div').style.backgroundColor = '#000000a0'
-        document.getElementById('crypto-div').onmouseleave = () => document.getElementById('crypto-div').style.backgroundColor = 'none'
     }, 3000)
 }
 
 initLoad()
 setInterval(()=> getTime(), 1000)
 
+document.getElementById('crypto-div').onmouseover = () => document.getElementById('crypto-div').style.backgroundColor = '#000000a0'
+document.getElementById('crypto-div').onmouseleave = () => document.getElementById('crypto-div').style.backgroundColor = '#00000040'
+
 /* document.getElementById('weather-div').addEventListener('click', test) */
+
 document.getElementById('info-btn').addEventListener('click', () => {
     document.getElementById('bigdiv').classList.toggle('blurred')
     document.getElementsByTagName('body')[0].classList.toggle('body-blurred')
     document.getElementById('info-modal').classList.toggle('show-modal')
 })
+
+document.addEventListener('keydown', function(e) {
+    if(e.key == 'Escape'){
+        if (document.getElementById('info-modal').classList.contains('show-modal')) {
+            console.log('esc while open')
+            document.getElementById('info-modal').classList.toggle('show-modal')
+            document.getElementById('bigdiv').classList.toggle('blurred')
+            document.getElementsByTagName('body')[0].classList.toggle('body-blurred')
+        }
+    }
+})
+
+function cryptoMouseOver() {
+    document.getElementById('crypto-div').style.backgroundColor = '#000000a0'
+}
+function cryptoMouseLeave() {
+    document.getElementById('crypto-div').style.backgroundColor = '00000040'
+}
