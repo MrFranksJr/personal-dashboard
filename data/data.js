5a8e18af855841cc20e7d0e387752e132ae74933
Franky Blondeel <112629948+MrFranksJr@users.noreply.github.com>
2023-07-17 13:36:00 +0200

    Delete data.js

--- data/data.js
+++
@@ -1,5 +0,0 @@
-export { aCweatherAPI, unSplashAPI, openWeatherAPI }
-
-const aCweatherAPI = `dfL96YXmXLlo4JvGFMHCTVs9AIuMI1OW`
-const openWeatherAPI = `41b4d6a775d3757be4b21e251b912bf7`
-const unSplashAPI = `-_WaTkHs23f_trveYoqlgf_SW6cKHl1E8acgWHKLejE`
