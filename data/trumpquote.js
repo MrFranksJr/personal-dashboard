02deeed556100b82d0ef430bde9c9409291ceb1d
Franky Jr Blondeel <frankyjr@me.com>
2025-01-05 14:59:40 +0100

    Replace Tronald Dump
    Discontinuing use of Tronald Dump quote generator due to shenanigans.
    Switching to whatdoestrumpthink.com

--- data/trumpquote.js
+++ data/trumpquote.js
@@ -1,14 +1,13 @@
 export { fetchTrumpQuote }
 
 async function fetchTrumpQuote() {
-    const trumpDataJson = await fetch(`https://api.tronalddump.io/random/quote`)
+    const trumpDataJson = await fetch(`https://api.whatdoestrumpthink.com/api/v1/quotes/random`)
     const trumpData = await trumpDataJson.json()
+    console.log(trumpData)
 
     document.getElementById('lowercenter').innerHTML = `
-    <a href='${trumpData._links.self.href}' alt='source of quote' target='_blank'>
-        <p class='quote'>" ${trumpData.value} "<br>
-        <span class='signed'> – Mr. Donald J. Trump – &nbsp;<br>45th President of the USA</span>
+        <p class='quote'>" ${trumpData.message} "<br>
+        <span class='signed'> – Mr. Donald J. Trump – &nbsp;<br><span class='trumpTitle'>45th & 47th President of the US</span></span>
         </p>
-    </a>
     `
 }
